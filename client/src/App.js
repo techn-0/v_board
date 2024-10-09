45693968b393375497203ddd0501c2e73ac88577
Alisser <gnlrjswjd@naver.com>
2024-10-09 16:37:27 +0900

    35:48

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
 
-const App = ()=>{
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import memories from "./images/image.png";
+
+const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
-}
+    <Container maxidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+          <img src={memories} alt="memories" hegth="60" />
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="strech"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <>
+      <h1>POSTS</h1>
+    </>
+  );
+};
+
+export default Post;
