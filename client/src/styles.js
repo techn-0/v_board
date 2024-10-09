21e1dd61aefc09fa77818763d4c64f2ca308dc22
Alisser <gnlrjswjd@naver.com>
2024-10-10 01:40:30 +0900

    보안강화

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@mui/styles";
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
     margin: "30px 0",
@@ -14,5 +14,11 @@ export default makeStyles(() => ({
   },
   image: {
     marginLeft: "15px",
+  },
+  //   모바일 접근성
+  [theme.breakpoints.down("sm")]: {
+    mainContainer: {
+      flexDirection: "column-reverse !important", // 우선순위 설정
+    },
   },
 }));
--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import dotenv from "dotenv";
+
 import postRoutes from "./routes/posts.js";
 
 
 const app = express();
+dotenv.config();
 
 // Body Parser 미들웨어 설정 (용량 제한을 "30mb"로 수정)
 // app.use(bodyParser.json({ limit: "50bm", extended: true }));
@@ -18,11 +21,11 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 // MongoDB 연결 URL
-const CONNECTION_URL =
-  "mongodb+srv://jsboard:jsboard13@cluster0.5wmd0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const CONNECTION_URL =
 
 // 서버 포트 설정
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 // MongoDB 연결 및 서버 시작
 mongoose
